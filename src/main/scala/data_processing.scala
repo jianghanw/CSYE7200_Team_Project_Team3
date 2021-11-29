4314dee08145a5c872e04e94b8bd07e81acbbe98
jianghanw <jiangh8@hotmail.com>
2021-11-28 23:35:06 -0500

    some data-preprocessing for the player.csv file

--- src/main/scala/data_processing.scala
+++ src/main/scala/data_processing.scala
@@ -1,5 +1,5 @@
 
-import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.functions.{bround, col}
 import org.apache.spark.sql.SparkSession
 
 
@@ -20,4 +20,14 @@ object data_processing extends App {
   val data = matchInfo.select("match_id","duration","radiant_win").withColumn("radiant_win", col("radiant_win").cast("int"))
 
   data.show()
+
+  val playerInfo = spark.read.format("csv")
+    .option("header", "true")
+    .option("inferschema", "true")
+    .load("src/main/resources/players.csv")
+
+  val playerData = playerInfo.select("match_id","hero_id","player_slot","gold_per_min","xp_per_min","kills","deaths","assists")
+  val kdaCal = (col("kills")+col("assists"))/col("deaths")
+  val kdaCom = playerData.withColumn("KDA",bround(kdaCal,2)).drop("kills","deaths","assists")
+  kdaCom.show()
 }
